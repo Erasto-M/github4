702ce17b182a8789737cc109537d713eb9794d63
Erastus Munyao kyalo <erastusmunya2022@gmail.com>
2023-03-29 13:30:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+    appBar: AppBAr(
+    );
+    );
   }
 }
 
